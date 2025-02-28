08911bc6eb323fa0e58e7b24ddd29eeeb3364b84
Gc8808 <526gmatos@frhsd.com>
2025-02-28 18:46:47 +0000

    Done

--- Quix.js
+++ Quix.js
@@ -3,9 +3,9 @@ function questionPrompt() {
     let userAns = prompt('When was Dwight D Eisenhower born?, How old was Dwight D Eisenhower when he passed? Answer in the format of mm/dd/yyyy,Age');
 
     if (userAns === '10/14/1890,78') {
-        respQue = 'Correct';  
+        respQue = 'Correct!';  
     } else {
-        respQue = 'Incorrect';  
+        respQue = 'Incorrect!';  
     }
 
     document.getElementById('Ans').innerHTML = respQue;  
